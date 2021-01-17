bc1e3ca6b5d01e5606b2d3685ab43f56daa25f5d
Flávio Bras da Fonseca <brasfonseca@gmail.com>
2021-01-17 13:37:30 -0300

    Update fields tables.

---
+++ CleanArch/CleanArch.Infra.Data/Migrations/VerboDB/20210109194450_Rename field and field lenght tableTParishCommunity..cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArch.Infra.Data.Migrations.VerboDB
+{
+    public partial class RenamefieldandfieldlenghttableTParishCommunity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Telefone",
+                table: "TParishCommunity");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telephone",
+                table: "TParishCommunity",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Telephone",
+                table: "TParishCommunity");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telefone",
+                table: "TParishCommunity",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true);
+        }
+    }
+}
