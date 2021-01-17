e6aa417cf95dfedfd121128020d47c407d957ed5
Flávio Bras da Fonseca <brasfonseca@gmail.com>
2021-01-17 17:54:25 -0300

    Teste com sucesso - SendGrid.

--- CleanArch/VerboCaptacao/Program.cs
+++ CleanArch/VerboCaptacao/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace VerboCaptacao
 {
@@ -13,7 +11,22 @@ namespace VerboCaptacao
     {
         public static void Main(string[] args)
         {
+            Execute().Wait();
             CreateHostBuilder(args).Build().Run();
+        }
+
+        static async Task Execute()
+        {
+            //var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("contato@verboproducoes.com", "Verbo Produções");
+            var subject = "Sistema de Captação de Recursos - Teste Email";
+            var to = new EmailAddress("brasfonseca@gmail.com", "Flávio Fonseca");
+            var plainTextContent = "vamos lá Flávio";
+            var htmlContent = "<strong>vamos!</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
