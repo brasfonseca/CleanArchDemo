80dbd853cbbc542c3b4d99a2a603888b4506f077
Flávio Bras da Fonseca <brasfonseca@gmail.com>
2021-01-18 13:06:52 -0300

    Scaffold Login

---
+++ CleanArch/VerboCaptacao/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VerboCaptacao.Data;
+
+[assembly: HostingStartup(typeof(VerboCaptacao.Areas.Identity.IdentityHostingStartup))]
+namespace VerboCaptacao.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<VerboCaptacaoContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("VerboCaptacaoContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<VerboCaptacaoContext>();
+            });
+        }
+    }
+}
